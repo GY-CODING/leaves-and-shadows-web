2194c00da2af5bf5780a11125af36498758b93ea
= <=>
2024-04-22 16:09:05 +0200

    fix: trying to solve problems

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Home (): JSX.Element {
   const router = useRouter()
-  router.push('/dashboard')
+
+  useEffect(() => {
+    router.replace('/dashboard')
+  }, [])
+
   return (
     <main className="w-screen h-screen flex flex-col bg-white">
     </main>
