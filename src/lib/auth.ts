717bab28c53e69cc2fb0ecd6442d2cf2d9768a36
= <=>
2024-04-22 10:55:00 +0200

    fix: delete some files

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,31 +1,28 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import type { DefaultSession, NextAuthOptions, Session } from 'next-auth'
+import type { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authConfig: NextAuthOptions = {
-  pages: {
-    signIn: '/dashboard/login'
-  },
-  session: {
-    strategy: 'jwt', // Use JSON Web Tokens (JWT) for session management
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-    updateAge: 24 * 60 * 60 // 24 hours
-  },
-  callbacks: {
-    session: async ({ session, user, token }): Promise<Session | DefaultSession> => {
-      if (session?.user) {
-        session.user.id = user.id
-        return await Promise.resolve(session)
-      }
-      return await Promise.resolve(session)
-    }
-  },
   providers: [
     GoogleProvider({
-      // Configure Google authentication provider with environment variables
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code'
+        }
+      }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn ({ account, profile }) {
+      if (account!.provider === 'google') {
+        return profile!.email_verified && profile!.email!.endsWith('@gmail.com')
+      }
+      return true // Do different verification for other providers that don't have `email_verified`
+    }
+  }
 }
