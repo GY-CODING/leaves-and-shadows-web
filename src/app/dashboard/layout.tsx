7666cd9ab3ca4ac2bf03e8d466f2e2e7712ba391
= <=>
2024-04-30 18:33:52 +0200

    chore: add responsive

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { cinzel } from '@/utils/fonts'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -14,6 +15,7 @@ export default function Layout ({ children }: Readonly<{ children: React.ReactNo
   const router = useRouter()
   const pathname = usePathname()
   const { data: session } = useSession()
+  const [layout, setLayout] = React.useState('')
 
   function pushToDashboard (): void {
     if (pathname !== '/dashboard') {
@@ -21,35 +23,43 @@ export default function Layout ({ children }: Readonly<{ children: React.ReactNo
       console.log('pushed')
     }
   }
+
+  useEffect(() => {
+    if (pathname === '/dashboard') {
+      setLayout('Login')
+    } else if (pathname === '/dashboard/login') {
+      setLayout('Dashboard')
+    }
+  }, [pathname])
+
   async function handleSignOut (): Promise<void> {
     await signOut()
     router.push('/dashboard')
   }
 
   return (
-        <>
-            <header className={'w-screen h-24 bg-zinc-900 flex flex-row self-start justify-self-star bg-opacity-20 backdrop:blur-'}>
-                <ul className="w-1/2 h-full flex flex-row items-center justify-start gap-6 pl-4">
-                    <Image onClick={pushToDashboard} src={logo} className="w-16 h-16 hover:cursor-pointer" alt="logo" width={100} height={100} />
-                    <div className="w-auto text-center">
-                        <Link className={`${cinzel.className} font-bold text-xl text-green-50`} href={'/dashboard/'} onClick={pushToDashboard}>Leaves & Shadows</Link>
-                        {/* <p className={`${cinzel.className} font-bold text-m text-green-50`}>Fall of the Gods</p> */}
+    <>
+      <header className={'w-screen sm:h-24 h-24 bg-zinc-900 flex justify-evenly md:justify-between flex-col md:flex-row self-start  bg-opacity-20 backdrop:blur-'}>
+        <ul className="flex flex-row justify-center align-center md:items-center text-center md:w-1/2 md:text-center md:justify-center md:align-center gap-4">
+          <Image onClick={pushToDashboard} src={logo} className="md:flex hidden w-16 h-16 hover:cursor-pointer ml-4" alt="logo" width={100} height={100} />
+          <div className="w-full text-center md:w-full h-full flex items-center justify-start">
+            <Link className={`${cinzel.className} hidden md:flex md:text-center md:items-center md:justify-center font-bold text-sm md:h-full text-green-50 sm:text-sm md:text-base lg:text-lg xl:text-xl`} href={'/dashboard/'} onClick={pushToDashboard}>Leaves & Shadows</Link>
+          </div>
 
-                    </div>
-                </ul>
-                <div className="w-1/2 h-full flex flex-row items-center justify-end pr-4 gap-4">
-                    {
-                        (session != null)
-                          ? <>
-                                <TemporaryDrawer logout={handleSignOut} />
-                            </>
-                          : <button className="w-40 h-12 rounded bg-zinc-950 flex text-white">
-                                <Link href="/dashboard/login" className={`${cinzel.className} text-green-40 0bg-zinc-950 hover:bg-green-500 hover:text-white w-full h-full rounded text-center items-center justify-center flex transition duration-500 ease-in-out`}>{'Login'}</Link>
-                            </button>
-                    }
-                </div>
-            </header>
-            {children}
-        </>
+        </ul>
+        <div className="w-full md:w-1/2 h-full flex flex-row items-center justify-center md:justify-end pr-4 gap-4">
+          {
+            (session != null)
+              ? <>
+                <TemporaryDrawer logout={handleSignOut} />
+              </>
+              : <button className="w-40 h-12 rounded bg-zinc-950 flex text-white">
+                <Link href={layout === 'Login' ? '/dashboard/login' : '/dashboard'} className={`${cinzel.className} text-green-40 0bg-zinc-950 hover:bg-green-500 hover:text-white w-full h-full rounded text-center items-center justify-center flex transition duration-500 ease-in-out`}>{layout || 'Login'}</Link>
+              </button>
+          }
+        </div>
+      </header>
+      {children}
+    </>
   )
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box } from '@mui/material'
 import Image from 'next/image'
 import logo from '../../assets/images/leaves_and_shadows.png'
@@ -7,6 +7,30 @@ import gif from '../../assets/video/bg.gif'
 import { cinzel, alkhemikal } from '@/utils/fonts'
 
 export default function Home (): JSX.Element {
+  useEffect(() => {
+    async function loadData (): Promise<any> {
+      try {
+        const response = await fetch('/api?email=gfigueras', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    void loadData()
+  }, [])
+
   return (
     <Box sx={{
       width: '100%',
@@ -29,8 +53,8 @@ export default function Home (): JSX.Element {
           className="blur"
         />
       </Box>
-      <Image src={logo} width={200} height={200} alt={''} />
-      <h3 className={`${alkhemikal.className} text-7xl text-white`}>Leaves & Shadows</h3>
+      <Image className='sm:w-2/12 w-1/3' src={logo} width={200} height={200} alt={''} />
+      <h3 className={`${alkhemikal.className} sm:text-7xl text-4xl text-white`}>Leaves & Shadows</h3>
         <button className={`${cinzel.className} w-32 h-12 rounded bg-green-600 text-white hover:bg-green-500 ease-in-out transition-all duration-500 `}>Download</button>
     </Box>
   )
