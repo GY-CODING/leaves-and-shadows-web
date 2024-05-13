7e1793adeb0e485804f9adc5df0f05b1087d7082
= <=>
2024-05-13 20:46:22 +0200

    fix: dbjs

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+  return new PrismaClient()
+}
 
-const globalForPrisma = globalThis;
+const globalForPrisma = globalThis
 
-const prisma = globalForPrisma.prisma || prismaClientSingleton();
+const prisma = globalForPrisma.prisma || prismaClientSingleton()
 
-export default prisma;
+export default prisma
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
 }
