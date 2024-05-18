dfeb41e700ba85914e41de1620785164ea4cabc6
= <=>
2024-05-18 22:32:13 +0200

    debugging

--- src/app/api/auth/[auth0]/auth.ts
+++ src/app/api/auth/[auth0]/auth.ts
@@ -1,5 +1,10 @@
 import { initAuth0 } from '@auth0/nextjs-auth0'
 
+console.log(`secret: ${process.env.AUTH0_SECRET}`)
+console.log(`issuerBaseURL: ${process.env.AUTH0_ISSUER_BASE_URL}`)
+console.log(`baseURL: ${process.env.AUTH0_BASE_URL}`)
+console.log(`clientID: ${process.env.AUTH0_CLIENT_ID}`)
+console.log(`clientSecret: ${process.env.AUTH0_CLIENT_SECRET}`)
 export default initAuth0({
   secret: process.env.AUTH0_SECRET,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
